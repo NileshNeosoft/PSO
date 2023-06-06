84bb028baf2ed0a0f7dd4333f3ed13d2e7c95b6b
NileshNeosoft <nilesh.neosoft@miraeassetcm.com>
2023-06-06 12:37:33 +0530

    Commit js

---
+++ sw.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: 'AIzaSyCwt9J-ToIRC-7FtZLJy64O5rdv7GDhFxE',
+    messagingSenderId: '787799882321',
+    appId: '1:787799882321:web:63af4ee959dbe708a85670',
+    projectId: 'uatmstockweb',
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K96BUJME5KPBLC3CHG0K7HQ2R4T2N0',
+    siteid: '6d48cffc034dc7c9785b5531aba34937'
+};
+importScripts('//cdnt.netcoresmartech.com/swv4.js');
